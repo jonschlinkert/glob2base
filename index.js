dbde767405113e8c577efe21b53a35548aee1097
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-01 19:30:13 -0400

    fragment cache

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 var path = require('path');
 var globParent = require('glob-parent');
+var cache = {};
 
 module.exports = function glob2base(glob) {
+  var type = typeof glob;
   var str = glob;
-  if (typeof glob === 'object' && glob.pattern) {
+
+  if (type === 'string' && cache.hasOwnProperty(glob)) {
+    return cache[glob];
+  }
+
+  if (type === 'object' && glob.pattern) {
     str = glob.pattern;
   }
+
   var parent = globParent(str);
   if (parent === str) {
     parent = path.dirname(str);
@@ -15,5 +23,5 @@ module.exports = function glob2base(glob) {
   if (parent[parent.length - 1] !== '/') {
     parent += '/';
   }
-  return parent;
+  return (cache[str] = parent);
 };
